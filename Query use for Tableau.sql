b1b23bf8bf1d1c34906315d9c986b26a37786d53
ThinhVuong98 <90743483+ThinhVuong98@users.noreply.github.com>
2021-09-15 22:23:22 +0700

    Add files via upload

--- Query use for Tableau.sql
+++ Query use for Tableau.sql
@@ -1,4 +1,4 @@
--- Queries used for Tableau Project
+--Queries used for Tableau Project
 
 -- 1.Global Cases and Deaths
 
@@ -39,5 +39,42 @@ From CovidProject..cases
 Where continent is not Null
 Group by location, date, population
 Order by location, date
+
+
+
+-- 5.Total population and cases in Vietnam
+
+Select location, population, max(total_cases) as TotalCases, 
+	cast((max(total_cases)/population) * 100 as decimal(16,2)) as InfectedPercent
+From CovidProject..cases
+Where continent is not Null
+	and location like 'Vietnam'
+Group by location, population
+
+
+
+-- 6.Vaccination and Population in Vietnam
+
+Select cas.location, cas.population, max(vac.total_vaccinations) as TotalVaccination, 
+	cast(max(vac.total_vaccinations)/population as decimal(16,2)) as VaccinatedPercent, 
+	cast(1-max(vac.total_vaccinations)/population as decimal(16,2)) as UnvaccinatedPercent
+From CovidProject..cases as cas
+Join CovidProject..vaccination as vac
+	on cas.location = vac.location
+	and cas.date = vac.date
+Where cas.continent is not Null
+	and cas.location like 'Vietnam'
+Group by cas.location, cas.population	
+
+
+
+-- 7.7 days moving average of infection case in Viet Nam
+Select location, date, total_cases, new_cases,
+	cast(avg(new_cases) over (partition by (location) order by location, date rows between 7 preceding and current row) as int) as '7DaysMovingAvg'
+From CovidProject..Cases
+Where continent is not null
+	and Location like 'Vietnam'
+Order by date
+
 
 
